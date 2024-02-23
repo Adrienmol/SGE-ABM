9a3b8ad297b7fdaba6158c07e16023d831634d3d
Adrienmol <adrianballester95@gmail.com>
2024-02-23 17:04:23 +0000

    Related

---
+++ .vscode-server/data/User/History/1f2082df/7cO0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class profesores(models.Model):
+    _name = 'tarea04abm.profesores'
+    _description = 'tarea04abm.profesores'
+
+    # Al no tener un "name", odoo usará el valor dado aquí:
+    _rec_name = 'nombreProfesor'
+
+    nombreProfesor = fields.Char("Nombre")
+    apellidoProfesor = fields.Char("Apellido")
+    modulosImparte = fields.One2many('tarea04abm.modulos', 'profesorImparte')
+
+    alumnos = fields.Many2many('tarea04abm.alumnos',related="alumnosMatriculados",string="Alumnos",readonly=True)
